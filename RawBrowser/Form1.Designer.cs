9b730741703a80aa416856142fdb07486574f794
Mark McCarron <info@marxbitware.co.uk>
2023-08-04 23:06:42 +0100

    Add project files.

---
+++ RawBrowser/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace RawBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            splitContainer2 = new SplitContainer();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer2).BeginInit();
+            splitContainer2.Panel1.SuspendLayout();
+            splitContainer2.Panel2.SuspendLayout();
+            splitContainer2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.FixedPanel = FixedPanel.Panel1;
+            splitContainer1.IsSplitterFixed = true;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(textBox2);
+            splitContainer1.Size = new Size(800, 450);
+            splitContainer1.SplitterDistance = 25;
+            splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            splitContainer2.Dock = DockStyle.Fill;
+            splitContainer2.Location = new Point(0, 0);
+            splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            splitContainer2.Panel1.Controls.Add(textBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            splitContainer2.Panel2.Controls.Add(button1);
+            splitContainer2.Size = new Size(800, 25);
+            splitContainer2.SplitterDistance = 570;
+            splitContainer2.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Fill;
+            textBox1.Location = new Point(0, 0);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(570, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "https://google.com";
+            textBox1.KeyPress += textBox1_KeyPress;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(226, 25);
+            button1.TabIndex = 0;
+            button1.Text = "Go";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Dock = DockStyle.Fill;
+            textBox2.Location = new Point(0, 0);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ScrollBars = ScrollBars.Vertical;
+            textBox2.Size = new Size(800, 421);
+            textBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(splitContainer1);
+            Name = "Form1";
+            Text = "Raw Browser v1.0.0";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            splitContainer2.Panel1.ResumeLayout(false);
+            splitContainer2.Panel1.PerformLayout();
+            splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer2).EndInit();
+            splitContainer2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private SplitContainer splitContainer2;
+        private TextBox textBox1;
+        private Button button1;
+        private TextBox textBox2;
+    }
+}
---
+++ RawBrowser/Form1.cs
@@ -0,0 +1,74 @@
+namespace RawBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+
+                    HttpResponseMessage hrm = httpClient.GetAsync(textBox1.Text).Result;
+
+                    textBox2.Text = "Headers:";
+                    textBox2.AppendText(Environment.NewLine);
+
+                    for (int i = 0; i < hrm.Headers.Count(); i++)
+                    {
+                        textBox2.AppendText(hrm.Headers.ElementAt(i).Key);
+
+                        for (int j = 0; j < hrm.Headers.ElementAt(i).Value.Count(); j++)
+                            textBox2.AppendText(hrm.Headers.ElementAt(i).Value.ElementAt(j) + " ");
+
+                        textBox2.AppendText(Environment.NewLine);
+                    }
+
+                    textBox2.AppendText(Environment.NewLine);
+                    textBox2.AppendText(Environment.NewLine);
+                    textBox2.AppendText("Content Headers:");
+                    textBox2.AppendText(Environment.NewLine);
+
+                    for (int i = 0; i < hrm.Content.Headers.Count(); i++)
+                    {
+                        textBox2.AppendText(hrm.Content.Headers.ElementAt(i).Key);
+
+                        for (int j = 0; j < hrm.Content.Headers.ElementAt(i).Value.Count(); j++)
+                            textBox2.AppendText(hrm.Content.Headers.ElementAt(i).Value.ElementAt(j) + " ");
+
+                        textBox2.AppendText(Environment.NewLine);
+                    }
+
+                    textBox2.AppendText(Environment.NewLine);
+                    textBox2.AppendText(Environment.NewLine);
+                    textBox2.AppendText("Content:");
+                    textBox2.AppendText(Environment.NewLine);
+
+                    textBox2.AppendText(Environment.NewLine);
+                    textBox2.AppendText(Environment.NewLine);
+                    textBox2.AppendText(hrm.Content.ReadAsStringAsync().Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                e.Handled = true;
+                button1_Click(sender, e);
+            }
+        }
+    }
+}
